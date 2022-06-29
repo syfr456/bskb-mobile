ea71effb386a04499ead66dfa8f9771ae0edd986
syfr456 <sekarsyafira225@gmail.com>
2022-06-29 15:15:08 +0700

    login rev

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
